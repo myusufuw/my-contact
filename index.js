b9e0e67de77bceeaafba19d03db35986417e03e8
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-03-19 16:42:35 +0700

    handle create-new-contact functionality

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const contactForm = document.getElementById('contact-form')
-const cancelButton = document.getElementById('cancel-button')
+// CONTACT LIST
+let contactList = JSON.parse(localStorage.getItem('my-contact')) || []
+// CONTACT ACTION BUTTONS
+const contactFormContainer = document.getElementById('contactFormContainer')
+const cancelButton = document.getElementById('cancelButton')
+const contactForm = document.getElementById('contactForm')
 
 const handleCreateNewContact = () => {
-  contactForm.style.display = 'block'
-  contactForm.style.opacity = 1
+  contactFormContainer.style.display = 'block'
+  contactFormContainer.style.opacity = 1
 }
 
 const handleCancelButtonClick = () => {
-  contactForm.style.display = 'none'
+  contactFormContainer.style.display = 'none'
+  contactForm.reset()
 }
+
+const saveContacts = () => {
+  localStorage.setItem('my-contact', JSON.stringify(contactList))
+}
+
+const handleSaveContact = () => {
+  const fullName = document.getElementById('fullName').value
+  const email = document.getElementById('email').value
+  const phoneNumber = document.getElementById('phoneNumber').value
+  const address = document.getElementById('address').value
+  const birthday = document.getElementById('birthday').value
+  const additionalNotes = document.getElementById('additionalNotes').value
+
+  contactList.push({
+    fullName,
+    email,
+    phoneNumber,
+    address,
+    birthday,
+    additionalNotes,
+  })
+
+  saveContacts()
+  contactFormContainer.style.display = 'none'
+  contactForm.reset()
+}
+
+console.log(contactList)
