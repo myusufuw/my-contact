301c83b529ef7d167dc1f5d52c7519f84140afc8
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-03-20 21:47:29 +0700

    show contact-list

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // CONTACT LIST
-let contactList = JSON.parse(localStorage.getItem('my-contact')) || []
+let rawContactList = JSON.parse(localStorage.getItem('my-contact')) || []
+const contactList = rawContactList.map((item, index) => {
+  return {
+    ...item,
+    isShown: true,
+  }
+})
+const contactListContainer = document.getElementById('contactListContainer')
 // CONTACT ACTION BUTTONS
 const contactFormContainer = document.getElementById('contactFormContainer')
 const cancelButton = document.getElementById('cancelButton')
@@ -41,4 +48,51 @@ const handleSaveContact = () => {
   contactForm.reset()
 }
 
+const renderContactList = () => {
+  contactListContainer.innerHTML = ''
+  contactList.forEach((item, index) => {
+    const cardTemplate = `
+    ${
+      item.isShown
+        ? `<div
+            class="bg-slate-800 p-3 rounded mb-3 flex flex-row justify-between items-center cursor-pointer"
+            onClick="renderDetailCard('card${index}')"
+          >
+            <p class=' text-lg'>${item.fullName}</p>
+            <div>
+              <button class='bg-blue-500 py-1 px-3 rounded hover:bg-blue-600 mr-2'> 
+                Edit 
+              </button>
+
+              <button class='bg-red-500 py-1 px-3 rounded hover:bg-red-600'> 
+                Delete 
+              </button>
+            </div>
+          </div>
+          <div
+            id="card${index}"
+            class="h-0 bg-slate-800 rounded mb-3 transition-[height] duration-500 overflow-hidden"
+          >
+            <div class=' m-4'>Hello</div>
+          </div>`
+        : ''
+    }
+    `
+    contactListContainer.innerHTML += cardTemplate
+  })
+}
+
+renderContactList()
+
 console.log(contactList)
+
+const renderDetailCard = (id) => {
+  const contactDetail = document.getElementById(id)
+
+  var style = window.getComputedStyle(contactDetail)
+  if (style.height === '0px') {
+    contactDetail.style.height = '100px'
+  } else {
+    contactDetail.style.height = '0px'
+  }
+}
